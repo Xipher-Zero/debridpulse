f4e02ecd64fd75ea4c83a90a0bb5e80ab9b9ee70
Xipher-Zero <68665477+Xipher-Zero@users.noreply.github.com>
2026-08-25 16:39:30 -0700

    Correct Downloads pagination chevron

--- frontend/static/ui-downloads-runtime.js
+++ frontend/static/ui-downloads-runtime.js
@@ -25,7 +25,7 @@
   function utilitySvg(name) {
     const paths = {
       chevronLeft: '<path d="m15 18-6-6 6-6"/>',
-      chevronRight: '<path d="m9 18 6 6-6-6"/>',
+      chevronRight: '<path d="m9 18 6-6-6-6"/>',
       refresh: '<path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16"/><path d="M3 21v-5h5"/><path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8"/><path d="M21 3v5h-5"/>'
     };
     const geometry = paths[name];
